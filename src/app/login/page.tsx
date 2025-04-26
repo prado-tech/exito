3a3a58c2b4828ccfa108e4dafc6e96a991df03ab
prado-tech <anascim43@gmail.com>
2025-04-26 10:05:58 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,24 @@
 "use client";
 
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+// import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-
-const auth = getAuth(); // <-- esta linha é essencial
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const router = useRouter();
 
-  const handleLogin = async () => {
+  /* const handleLogin = async () => {
     try {
+      const auth = getAuth();
       await signInWithEmailAndPassword(auth, email, senha);
       router.push("/admin");
     } catch (error) {
       console.error(error);
       alert("Erro ao logar. Verifique email e senha.");
     }
-  };
+  }; */
 
   return (
     <div className="p-8">
@@ -38,7 +37,10 @@ export default function Login() {
         value={senha}
         onChange={(e) => setSenha(e.target.value)}
       />
-      <button onClick={handleLogin} className="bg-blue-600 text-white px-4 py-2 rounded">
+      <button 
+        // onClick={handleLogin} 
+        className="bg-blue-600 text-white px-4 py-2 rounded"
+      >
         Entrar
       </button>
     </div>
